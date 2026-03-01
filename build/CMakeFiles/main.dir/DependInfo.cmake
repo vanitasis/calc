a7d4443def06da563a8c902430cdfc200d98d5c5
vanitasis <alandanrian77@gmail.com>
2026-03-02 05:53:32 +0700

    gitignore

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vanitasis/Documents/latihan_c/projek_c/src/Input.c" "CMakeFiles/main.dir/src/Input.c.o" "gcc" "CMakeFiles/main.dir/src/Input.c.o.d"
+  "/home/vanitasis/Documents/latihan_c/projek_c/src/StandardOp.c" "CMakeFiles/main.dir/src/StandardOp.c.o" "gcc" "CMakeFiles/main.dir/src/StandardOp.c.o.d"
+  "/home/vanitasis/Documents/latihan_c/projek_c/src/main.c" "CMakeFiles/main.dir/src/main.c.o" "gcc" "CMakeFiles/main.dir/src/main.c.o.d"
+  "" "main" "gcc" "CMakeFiles/main.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
